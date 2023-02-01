c0088a1f2cfbd87abafbbef8d1da1d812df77b2b
Matt Ventrelli <mventrelli@hawk.iit.edu>
2023-02-01 12:36:04 -0600

    Initial Commit

---
+++ recursionRelations/recursionRelationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  recursionRelationsApp.swift
+//  recursionRelations
+//
+//  Created by IIT PHYS 440 on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct recursionRelationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
