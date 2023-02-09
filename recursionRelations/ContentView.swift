bfc191cc9ab29fd9d701a95b0a0eaa61dd739c29
Matt Ventrelli <mventrelli@hawk.iit.edu>
2023-02-09 13:33:03 -0600

    gffgdfg

--- recursionRelations/ContentView.swift
+++ recursionRelations/ContentView.swift
@@ -1,26 +1,77 @@
-//
-//  ContentView.swift
-//  recursionRelations
-//
-//  Created by IIT PHYS 440 on 2/1/23.
-//
-
 import SwiftUI
+import Foundation
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+func besselJ(l: Int, x: Double) -> Double {
+    // Implementation of the Bessel function
+    struct ContentView: View {
+        var body: some View {
+            Text("Bessel functions")
+                .font(.largeTitle)
+                .padding()
+            Text("The result of the Bessel function is: \(besselJ(l: 4, x: 2.0))")
+                .font(.title)
+                .padding()
         }
-        .padding()
+        
+        func besselJ(l: Int, x: Double) -> Double {
+            if l == 0 {
+                return J0(x)
+            } else if l == 1 {
+                return J1(x)
+            } else {
+                let Jlminus1 = besselJ(l: l - 1, x: x)
+                let Jlminus2 = besselJ(l: l - 2, x: x)
+                return ((2 * Double(l) - 1) / x) * Jlminus1 - Jlminus2
+            }
+        }
+        
+        func J0(_ x: Double) -> Double {
+            if x == 0 {
+                return 1
+            }
+            let p = [1.0, 3.5156229, 3.0899424, 1.2067492, 0.2659732, 0.0360768, 0.0045813]
+            let q = [0.5, 0.87890594, 0.51498869, 0.15084934, 0.02658773, 0.00301532, 0.00032411]
+            let z = x * x
+            var sum = p[0]
+            for i in 1..<7 {
+                sum += p[i] * pow(z, Double(i)) / q[i]
+            }
+            return sum * exp(-z)
+        }
+        
+        func J1(_ x: Double) -> Double {
+            if x == 0 {
+                return 0
+            }
+            let p = [0.5, 0.87890594, 0.51498869, 0.15084934, 0.02658773, 0.00301532, 0.00032411]
+            let q = [1.0, 2.5156229, 1.6339158, 0.5213763, 0.07757657, 0.00958388, 0.00015355]
+            let z = x * x
+            var sum = x * p[0]
+            for i in 1..<7 {
+                sum += p[i] * pow(z, Double(i - 1)) / q[i]
+            }
+            return sum * exp(-z)
+        }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct Convergence: View {
+        var l: Int
+        var x: Double
+        
+        var body: some View {
+            Text("Convergence analysis")
+                .font(.largeTitle)
+                .padding()
+            Text("The result of the convergence analysis is: \(analyzeConvergence(l: l, x: x))")
+                .font(.title)
+                .padding()
+        }
+        
+        func analyzeConvergence(l: Int, x: Double) -> String {
+            let tolerance = 1e-12
+            let maxIterations = 100
+            var previousResult = 0.0
+            var result = besselJ(l: l, x: x)
+        }
     }
 }
